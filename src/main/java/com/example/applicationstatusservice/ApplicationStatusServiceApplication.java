2fad8f83b76256c2d9a4a792e71161168f8413f2
Yas Asghari <100201381+yasasghari@users.noreply.github.com>
2024-02-13 18:51:29 +0100

    Project initialised + CI/CD

---
+++ src/main/java/com/example/applicationstatusservice/ApplicationStatusServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.applicationstatusservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationStatusServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationStatusServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/applicationstatusservice/ApplicationStatusServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.applicationstatusservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationStatusServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
