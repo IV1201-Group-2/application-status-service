a83dfd18cc877f83411adab4be422cbfb4f8aa7f
Yas Asghari <100201381+yasasghari@users.noreply.github.com>
2024-02-20 14:18:18 +0100

    Updated applicationstatusDTO

--- src/main/java/com/example/applicationstatusservice/model/dto/ApplicationStatusDTO.java
+++ src/main/java/com/example/applicationstatusservice/model/dto/ApplicationStatusDTO.java
@@ -1,15 +1,13 @@
 package com.example.applicationstatusservice.model.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class ApplicationStatusDTO {
 
     private final Long person_id;
     private final String status;
 
-    public ApplicationStatusDTO(Long personId, String status) {
-        person_id = personId;
-        this.status = status;
-    }
 }
